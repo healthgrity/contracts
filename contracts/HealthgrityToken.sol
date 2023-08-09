faea46968a521b21952d2a63bdea63e62892ad05
Roland BARY <roland.bary@monecho.com>
2023-08-10 00:33:25 +0200

    updating htg token contract

--- contracts/HealthgrityToken.sol
+++ contracts/HealthgrityToken.sol
@@ -6,15 +6,15 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Snapshot.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20FlashMint.sol";
 
 /// @custom:security-contact rbary.lite@gmail.com
 contract HealthgrityToken is ERC20, ERC20Burnable, ERC20Snapshot, Ownable, Pausable, ERC20Permit, ERC20Votes, ERC20FlashMint {
     constructor()
-    ERC20("Healthgrity Token", "HTG")
-    ERC20Permit("Healthgrity Token")
+    ERC20("HealthgrityToken", "HTG")
+    ERC20Permit("HealthgrityToken")
     {
         _mint(msg.sender, 100000000 * 10 ** decimals());
     }
